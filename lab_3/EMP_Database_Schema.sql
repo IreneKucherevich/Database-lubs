e385d16245bee3c3acb084a086c4375c1a0b1c41
Irina <33181611+IreneKucherevich@users.noreply.github.com>
2019-11-09 22:28:19 +0300

    Add tasks comments lab 3

--- lab_3/EMP_Database_Schema.sql
+++ lab_3/EMP_Database_Schema.sql
@@ -201,46 +201,48 @@ INSERT INTO SALARY VALUES
 
 -----------------------------QUERIES_LUB_1----------------------------------
 
+--Выдать информацию о местоположении отдела продаж (SALES) компании.			
 SELECT DEPTNAME, DEPTADDR FROM DEPT WHERE DEPTNAME = 'SALES';
 
-
+--Выдать информацию об отделах, расположенных в Chicago и New York.
 SELECT * FROM DEPT WHERE DEPTADDR = 'CHICAGO' OR DEPTADDR = 'NEW YORK';
 
-
+--Найти минимальную заработную плату, начисленную в 2009 году.
 SELECT MIN(SALVALUE) FROM SALARY WHERE YEAR = 2009;
 
-
+--Выдать информацию обо всех работниках, родившихся не позднее 1 января 1960 года.
 SELECT * FROM EMP WHERE BIRTHDATE <= to_date('1-1-1960','dd-mm-yyyy');
 
-
+--Подсчитать число работников, сведения о которых имеются в базе данных .
 SELECT COUNT(*) FROM EMP;
 
-
+--Найти работников, чьё имя состоит из одного слова. Имена выдать на нижнем регистре, с удалением  стоящей справа буквы t.
 SELECT RTRIM(LOWER(EMPNAME), 't') FROM EMP WHERE EMPNAME NOT LIKE '% %';
 
-
+--Выдать информацию о работниках, указав дату рождения в формате день(число), месяц(название), год(название).
 SELECT EMPNO, EMPNAME, TO_CHAR(BIRTHDATE, 'DD-MONTH-YEAR','NLS_DATE_LANGUAGE = RUSSIAN') FROM EMP;
 
-
+--Тоже, но год числом.
 SELECT EMPNO, EMPNAME, TO_CHAR(BIRTHDATE,'DD-MONTH-YYYY') FROM EMP;
 
-
+--Выдать информацию о должностях, изменив названия должности “CLERK” и “DRIVER” на “WORKER”.
 SELECT DECODE(JOBNAME, 'DRIVER', 'WORKER', 'CLERK', 'WORKER', JOBNAME) FROM JOB;
 
-
+--Определите среднюю зарплату за годы, в которые были начисления не менее чем за три месяца.
 SELECT YEAR, ROUND(AVG(SALVALUE),2) FROM SALARY GROUP BY YEAR HAVING COUNT(MONTH)>= 3;
 
-
+--Выведете ведомость получения зарплаты с указанием имен служащих.
 SELECT EMP.EMPNAME, SALARY.MONTH, SALARY.YEAR, SALARY.SALVALUE FROM EMP,SALARY WHERE SALARY.EMPNO = EMP.EMPNO;
 
-
+--Укажите сведения о начислении сотрудникам зарплаты, попадающей в вилку:
+--минимальный оклад по должности - минимальный оклад по должности плюс пятьсот. Укажите соответствующую вилке должность.
 SELECT EMP.EMPNAME, JOB.JOBNAME, SALARY.SALVALUE, JOB.MINSALARY FROM SALARY 
     INNER JOIN EMP ON SALARY.EMPNO = EMP.EMPNO 
     INNER JOIN CAREER ON CAREER.EMPNO = EMP.EMPNO 
     INNER JOIN JOB ON JOB.JOBNO = CAREER.JOBNO
     WHERE SALARY.SALVALUE > JOB.MINSALARY AND SALARY.SALVALUE < JOB.MINSALARY + 500;
     
-    
+-- Укажите сведения о заработной плате, совпадающей с минимальными окладами по должностям (с указанием этих должностей).   
 SELECT EMP.EMPNAME, SALARY.SALVALUE, JOB.MINSALARY, JOB.JOBNAME FROM SALARY  
     INNER JOIN EMP ON (SALARY.EMPNO = EMP.EMPNO) 
     INNER JOIN CAREER ON (EMP.EMPNO = CAREER.EMPNO) 
@@ -251,28 +253,28 @@ SELECT EMP.EMPNAME, SALARY.SALVALUE, JOB.MINSALARY, JOB.JOBNAME FROM SALARY
 --SELECT EMP.EMPNAME, SALARY.SALVALUE, JOB.MINSALARY, JOB.JOBNAME FROM SALARY, EMP, CAREER, JOB 
 --WHERE SALARY.EMPNO = EMP.EMPNO AND EMP.EMPNO = CAREER.EMPNO AND CAREER.JOBNO = JOB.JOBNO;      
 
-
+--Найдите сведения о карьере сотрудников с указанием вместо номера сотрудника его имени.
 SELECT EMP.EMPNAME, CAREER.JOBNO, CAREER.DEPTNO, CAREER.STARTDATE, CAREER.ENDDATE FROM EMP NATURAL JOIN CAREER;
 
-
+--Найдите сведения о карьере сотрудников с указанием вместо номера сотрудника его имени.
 SELECT EMP.EMPNAME, CAREER.JOBNO, CAREER.DEPTNO, CAREER.STARTDATE, CAREER.ENDDATE FROM EMP 
     INNER JOIN CAREER ON EMP.EMPNO = CAREER.EMPNO;
     
-
+--Выдайте сведения о карьере сотрудников с указанием их имён, наименования должности, и названия отдела.
 SELECT EMP.EMPNAME, JOB.JOBNAME, DEPT.DEPTNAME, CAREER.STARTDATE, CAREER.ENDDATE FROM EMP
     INNER JOIN CAREER ON CAREER.EMPNO = EMP.EMPNO
     INNER JOIN DEPT ON CAREER.DEPTNO = DEPT.DEPTNO
     INNER JOIN JOB ON JOB.JOBNO = CAREER.JOBNO;
     
-
+--Выдайте сведения о карьере сотрудников с указанием их имён.
 SELECT EMP.EMPNAME, CAREER.STARTDATE, CAREER.ENDDATE FROM EMP 
     LEFT OUTER JOIN CAREER ON EMP.EMPNO = CAREER.EMPNO ;   
 
-       
+--Составьте запрос с использованием противоположного вида соединения.       
 SELECT EMP.EMPNAME, CAREER.STARTDATE, CAREER.ENDDATE FROM EMP
     RIGHT OUTER JOIN CAREER ON EMP.EMPNO = CAREER.EMPNO ;   
 
-
+--Составьте запрос с использованием полного внешнего соединения.
 SELECT EMP.EMPNAME, CAREER.STARTDATE, CAREER.ENDDATE FROM EMP
     FULL OUTER JOIN CAREER ON EMP.EMPNO = CAREER.EMPNO ;     
      
@@ -280,12 +282,13 @@ SELECT EMP.EMPNAME, CAREER.STARTDATE, CAREER.ENDDATE FROM EMP
 
 -----------------------------QUERIES_LUB_2----------------------------------
   
-
+--Найти имена сотрудников, получивших за годы начисления зарплаты минимальную зарплату.
 SELECT EMP.EMPNAME FROM EMP 
     INNER JOIN SALARY ON EMP.EMPNO = SALARY.EMPNO
     WHERE (SELECT MIN(SALVALUE) FROM SALARY) = SALARY.SALVALUE;
 
-    
+--Найти имена сотрудников, работавших или работающих в тех же отделах, в которых работал или работает сотрудник 
+--с именем RICHARD MARTIN.    
 SELECT EMPNAME FROM EMP
     WHERE EMPNO IN (SELECT DISTINCT EMPNO FROM CAREER
         WHERE DEPTNO IN (SELECT DEPTNO FROM CAREER
@@ -293,7 +296,7 @@ SELECT EMPNAME FROM EMP
                 WHERE EMPNAME = 'RICHARD MARTIN')))
     AND NOT EMPNAME = 'RICHARD MARTIN';
                 
-
+--Найти имена сотрудников, работавших или работающих в тех же отделах и должностях, что и сотрудник 'RICHARD MARTIN'.
 SELECT EMPNAME FROM EMP
     WHERE EMPNO IN (SELECT DISTINCT EMPNO FROM CAREER
         WHERE DEPTNO IN (SELECT DEPTNO FROM CAREER
@@ -305,20 +308,21 @@ SELECT EMPNAME FROM EMP
                 WHERE EMPNAME = 'RICHARD MARTIN')))
     AND NOT EMPNAME = 'RICHARD MARTIN';
      
-
+--Найти сведения о номерах сотрудников, получивших за какой-либо месяц зарплату большую, чем средняя зарплата за 2007 г. 
+--или большую, чем средняя зарплата за 2008г.
 SELECT DISTINCT EMPNO FROM SALARY 
     WHERE SALVALUE > ANY(SELECT AVG(SALVALUE) FROM SALARY GROUP BY YEAR HAVING YEAR = 2007 OR YEAR = 2008);
      
- 
+--Найти сведения о номерах сотрудников, получивших зарплату за какой-либо месяц большую, чем средние зарплаты за все годы начислений. 
 SELECT DISTINCT EMPNO FROM SALARY 
     WHERE SALVALUE > ALL(SELECT AVG(SALVALUE) FROM SALARY GROUP BY YEAR);
     
-
+--Определить годы, в которые начисленная средняя зарплата была больше средней зарплаты за все годы начислений.
 SELECT YEAR FROM SALARY 
     GROUP BY YEAR 
     HAVING AVG(SALVALUE) > (SELECT AVG(SALVALUE) FROM SALARY);
 
-
+--Определить номера отделов, в которых работали или работают сотрудники, имеющие начисления зарплаты.
 SELECT DISTINCT DEPTNO
     FROM DEPT
     WHERE DEPTNO IN (
@@ -327,7 +331,7 @@ SELECT DISTINCT DEPTNO
         WHERE S.SALVALUE IS NOT NULL)
     ORDER BY DEPTNO;
 
-
+--Определить номера отделов, в которых работали или работают сотрудники, имеющие начисления зарплаты.
 SELECT DEPTNO
     FROM DEPT
     WHERE EXISTS (
@@ -335,7 +339,7 @@ SELECT DEPTNO
         FROM CAREER NATURAL JOIN EMP NATURAL JOIN SALARY
         WHERE DEPT.DEPTNO = CAREER.DEPTNO);
 
-
+--Определить номера отделов, для сотрудников которых не начислялась зарплата.
 SELECT DEPTNO
     FROM DEPT
     WHERE NOT EXISTS (
@@ -343,14 +347,15 @@ SELECT DEPTNO
         FROM CAREER NATURAL JOIN EMP NATURAL JOIN SALARY
         WHERE DEPT.DEPTNO = CAREER.DEPTNO);
 
-
+--Вывести сведения о карьере сотрудников с указанием названий и адресов отделов вместо номеров отделов.
 SELECT CAREER.EMPNO, DEPT.DEPTNAME, DEPT.DEPTADDR, CAREER.STARTDATE, CAREER.ENDDATE FROM CAREER
     NATURAL JOIN DEPT; 
 
-
+--Определить целую часть средних зарплат, по годам начисления.
 SELECT YEAR, CAST(AVG(SALVALUE) AS INT) FROM SALARY GROUP BY YEAR ORDER BY YEAR;
 
-
+--Разделите сотрудников на возрастные группы: 
+--A) возраст 20-30 лет; B) 31-40 лет; C) 41-50; D) 51-60 или возраст не определён.
 SELECT EMPNO, EMPNAME, 
     CASE
         WHEN MONTHS_BETWEEN (SYSDATE, BIRTHDATE) / 12 BETWEEN 20 AND 30 THEN '20-30'
@@ -362,7 +367,7 @@ SELECT EMPNO, EMPNAME,
         AS AGE_GROUP
     FROM EMP;
     
-
+--Перекодируйте номера отделов, добавив перед номером отдела буквы BI для номеров <=20, буквы LN для номеров >=30.
 SELECT
     CASE
         WHEN DEPTNO <= 20 THEN CONCAT('BI', CAST (DEPTNO AS VARCHAR(10)))
@@ -371,10 +376,7 @@ SELECT
     AS DEPTNO, DEPTNAME, DEPTADDR
     FROM DEPT;
     
-    
+--Выдать информацию о сотрудниках из таблицы EMP, заменив отсутствие данного о дате рождения датой '01-01-1000'.    
 SELECT EMPNO, EMPNAME,
     COALESCE(BIRTHDATE, to_date('01-01-1000', 'dd-mm-yyyy')) AS BIRTHDATE
     FROM EMP
-
-
-
